5209214feaf15674bb03307a5f4a4c55fd47d9e8
João Melo <112974292+devjoaomelo@users.noreply.github.com>
2022-09-20 12:31:02 -0300

    done

--- Desafios-html-css-js/jogo-advinhacao/main.js
+++ Desafios-html-css-js/jogo-advinhacao/main.js
@@ -1,16 +1,39 @@
-const randomNumber = Math.round(Math.random() * 10)
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+let randomNumber = Math.round(Math.random() * 10)
 console.log(randomNumber)
 let xAttemps = 1
 
-function handleClick(event){
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+// função callback
+function handleTryClick(event){
     event.preventDefault()
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
-        
-        document.querySelector(".screen2 h2").innerText = `acertou em ${xAttemps} tentativas`
+        toggleScreen()
+        screen2.querySelector(".screen2 h2").innerText = `acertou em ${xAttemps} tentativas`
+        randomNumber = Math.round(Math.random() * 10)
     }
+    inputNumber.value = ""
     xAttemps++
 }
+
+function handleResetClick(){
+    toggleScreen()
+    xAttemps = 1
+}
+
+function toggleScreen(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+
+}
+
