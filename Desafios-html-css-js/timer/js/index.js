4864d24c20d35ac4a8db4c67a67483a038092372
João Melo <112974292+devjoaomelo@users.noreply.github.com>
2022-09-22 20:14:25 -0300

    timer done

---
+++ Desafios-html-css-js/timer/js/index.js
@@ -0,0 +1,29 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js";
+import Events from "./events.js";
+import { 
+  buttonPause, 
+  buttonPlay, 
+  buttonSet, 
+  buttonStop, 
+  minutesDisplay, 
+  secondsDisplay 
+ } from "./elements.js";
+
+const sound = Sound();
+
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop
+});
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+})
+
+Events({ controls, timer, sound });
